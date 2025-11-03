fc9c9e1e42ef7241fa6ade1cf4f6e9093338a004
Alexandre-Cholat <165085048+Alexandre-Cholat@users.noreply.github.com>
2025-11-03 20:24:59 +0100

    FirstApp test

--- smart-EV3/src/FirstApp.java
+++ smart-EV3/src/FirstApp.java
@@ -1,4 +1,20 @@
+import lejos.hardware.BrickFinder;
+import lejos.hardware.lcd.GraphicsLCD;
+import lejos.utility.Delay;
+
 
 public class FirstApp {
+	
+	public static void main(String[] args) {
+		GraphicsLCD g= BrickFinder.getDefault().getGraphicsLCD();
+		
+		g.drawString("Hello Fuckers", 0, 0, 0);
+		
+		Delay.msDelay(5000);
+		
+	}
+	
 
 }
+
+
